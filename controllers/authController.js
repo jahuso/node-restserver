95e898ee02e4610606595853a9b58643403ba46f
Jose Ahumada <jahumada@prodriver.com>
2023-03-02 10:39:40 -0500

    Added Google Authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const {response}= require('express');
 const bcryptjs= require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generarJWT');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login=async(req,res = response)=> {
     const{correo,password} = req.body;
@@ -48,7 +49,54 @@ const login=async(req,res = response)=> {
     
 }
 
+const googleSignIn = async(req,res = response)=>{
+    const {id_token} = req.body;
+    
+    try {
+
+        const {correo,nombre,img} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        if (!usuario) {
+            //Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password:';P',
+                img,
+                rol:'USER_ROLE',
+                google:true
+            };
+
+            usuario = new Usuario(data);
+            console.log(usuario);
+            await usuario.save();
+        }
+
+       //si el usuario no esta en DB
+       if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'hable con el admin, usuario bloqueado'
+            });
+       }
+
+       //generar JWT
+       const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            msg: 'El Token google invalido'
+        })
+    }
+}
 
 module.exports ={
-    login
+    login,
+    googleSignIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const {Router} = require('express');
 const {check} = require('express-validator');
 
-const{login} = require('../controllers/authController');
+const{login, googleSignIn} = require('../controllers/authController');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -11,6 +11,11 @@ router.post('/login',[
     check('password','La contrasena es obligatoria').not().isEmpty(),
     validarCampos
 ],login);
+
+router.post('/google',[
+    check('id_token','id_token es necesario').not().isEmpty(),
+    validarCampos
+],googleSignIn);
 
 router.post('/login',login);
 
